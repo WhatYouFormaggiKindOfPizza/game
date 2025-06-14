fec0e9ce7549a73c87a5d2144865aec08e499a50
Oskar Legner <oskarlegner@gmail.com>
2025-06-14 20:53:50 +0200

    Clearify GM logs

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -55,10 +55,6 @@ func load_posts_from_json() -> void:
 		push_error("Error parsing JSON: ", posts_json_path)
 		return
 
-
-	if show_logs:
-		print("Loaded posts from JSON: " + str(json_as_dict))
-
 	for post_data in json_as_dict:
 		var post = Post.new(post_data)
 		posts.append(post)
@@ -66,11 +62,19 @@ func load_posts_from_json() -> void:
 # logs
 
 func log_ready() -> void:
-	print("GameManager is ready with " + str(entity_groups.size()) + " entity groups and " + str(kings.size()) + " kings.")
-	print("Loaded " + str(posts.size()) + " posts from JSON.")
+	print("GameManager is ready")
+	
+	print("--- Entity Groups ---")
+	print("Loaded " + str(entity_groups.size()) + " entity groups from scene.")
 	for e in entity_groups:
 		print("EntityGroup: " + e.group_name + " with ID: " + str(e.id))
+
+	print("--- Kings ---")
+	print("Loaded " + str(kings.size()) + " kings from scene.")
 	for k in kings:
 		print("King: " + k.king_name + " with ID: " + str(k.id) + " and is_player: " + str(k.is_player))
+		
+	print("--- Posts ---")
+	print("Loaded " + str(posts.size()) + " posts from JSON.")
 	for p in posts:
 		print("Post: " + p.title + " with ID: " + str(p.id))

30d45441db2d894a1d93582027ed90a3070f6c86
Oskar Legner <oskarlegner@gmail.com>
2025-06-14 14:26:41 +0200

    Add kings and few groups

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,20 +1,45 @@
 class_name GameManager extends Node
 
+@onready var kings_parent: Node = %Pretenders
+@onready var entity_groups_parent: Node = %EntityGroups
+
 
 var entity_groups: Array[EntityGroup]
-var kings = Array[King]
-
+var kings: Array[King]
 
 
 func _init() -> void:
-    # Get all groups from child nodes
-    # - kings
-    # - entity groups
-    
-    pass
+	
+	pass
+
 
 
 func init_entity_groups() -> void:
-    for e in entity_groups:
-        e.init(kings)
+	for e in entity_groups:
+		e.init(kings)
 
+		
+func _ready() -> void:
+	print(entity_groups_parent.name)
+
+	for child in entity_groups_parent.get_children():
+		if child is EntityGroup:
+			entity_groups.append(child)
+		else:
+			push_error("Unknown child type: " + str(child))
+	
+	for child in kings_parent.get_children():
+		if child is King:
+			kings.append(child)
+		else:
+			push_error("Unknown child type: " + str(child))
+
+	init_entity_groups()
+
+
+	print("GameManager is ready with " + str(entity_groups.size()) + " entity groups and " + str(kings.size()) + " kings.")
+	for e in entity_groups:
+		print("EntityGroup: " + e.group_name + " with ID: " + str(e.id))
+	for k in kings:
+		print("King: " + k.king_name + " with ID: " + str(k.id) + " and is_player: " + str(k.is_player))
+	pass
--- scripts/king.gd
+++ scripts/king.gd
@@ -1,6 +1,22 @@
 class_name King extends Node
 
+static var next_id: int = 0
+
+@export var king_name: String
+@export var icon: Texture2D
+@export var color: Color
+@export var is_player: bool
+
+
 var id: int
-var king_name
-var is_player
+
+func _init() -> void:
+    id = next_id
+    next_id += 1
+
+
+
+
+
+
 

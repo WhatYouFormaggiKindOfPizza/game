c223a1beda4ac2810bf5282cbffa332f6082aa8e
Nvk-Dev <a@a.a>
2025-06-15 23:29:11 +0200

    Final

--- scripts/screens/game_screen.gd
+++ scripts/screens/game_screen.gd
@@ -5,6 +5,7 @@ class_name GameScreen extends Node2D
 
 
 @onready var rel_bar_container: RelBarContainer = %RelBarContainer
+@onready var candidate_supp_container: RelBarContainer = %CandidateSupportContainer
 @onready var posts_container: HBoxContainer = %PostsContainer
 @onready var days_until: DaysUntil = %DaysUntil
 @onready var phone: Phone = %Phone
@@ -12,3 +13,4 @@ class_name GameScreen extends Node2D
 func init(_gm: GameManager) -> void:
 	self.game_manager = _gm
 	rel_bar_container.init(_gm.entity_groups, _gm)
+	candidate_supp_container.init(_gm.entity_groups, _gm)

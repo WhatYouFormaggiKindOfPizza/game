b965a91758caa853ac5c0e807788adf34fa43046
Oskar Legner <oskarlegner@gmail.com>
2025-06-15 15:40:56 +0200

    Show start screen on start

--- scenes/start_screen.gd
+++ scenes/start_screen.gd
@@ -2,5 +2,8 @@ class_name StartScreen extends Control
 
 @onready var game_manager: GameManager = %GameManager
 
+func _ready() -> void:
+    show()
+
 func _on_button_pressed() -> void:
     game_manager.start_game()
